7a09ba1f25ebbfb21e1a2776b0f0f5aec6cae693
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-03-06 16:16:01 +0100

    Refactor AppMain Refactor the bootstrap instalation component the main layout

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,14 +1,40 @@
 import DigitalComics from '../assets/img/buy-comics-digital-comics.png'
-
-export default function AppMain(){
-    return(
+import DcMerchandise from '/buy-comics-merchandise.png'
+import SubScription from '/buy-comics-subscriptions.png'
+import ComicShopLocator from '/buy-comics-shop-locator.png'
+import DcPowerVisa from '/buy-dc-power-visa.svg'
+export default function AppMain() {
+    return (
         <main>
-            <div className="contaier">
-                <div>
-                    <img src={DigitalComics} alt="digital Comics" />Digital Comics
-
+            <div className='bg-black'>
+                <div className="container">
+                    <div className='row '>
+                        <div className='col text-white'><h1>Content goes Here</h1></div>
+                    </div>
                 </div>
             </div>
+            <div className='bg-primary'>
+                <div className="container">
+                    <div className='row' >
+                        <div className='col'>
+                            <img className='immagine' src={DigitalComics} alt="digital Comics" /> <span>Digital Comics</span>
+                        </div>
+                        <div className='col'>
+                            <img className='immagine' src={DcMerchandise} alt="digital Comics" /> <span>Digital Comics</span>
+                        </div>
+                        <div className='col'>
+                            <img className='immagine' src={SubScription} alt="digital Comics" /> <span>Digital Comics</span>
+                        </div>
+                        <div className='col'>
+                            <img className='immagine' src={ComicShopLocator} alt="digital Comics" /> <span>Digital Comics</span>
+                        </div>
+                        <div className='col'>
+                            <img className='immagine' src={DcPowerVisa} alt="digital Comics" /> <span>Digital Comics</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </main>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 import './index.css'
 import App from './App.jsx'
